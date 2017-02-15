43d6a92e43a0461beb38a9fb24755488b71fadc6
Marco Peyrot <mpeyrotc@dhcp-10-16-177-177.mty.itesm.mx>
2017-02-14 23:14:06 -0600

    typo in index.html in views.py

--- condominios/views.py
+++ condominios/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     context = {}
-    return render(request, "index.hmtl", context)
+    return render(request, "index.html", context)

19dfe2a74c0ec022aa9b47c3b6e18b201764ce3f
Marco Peyrot <mpeyrotc@Marcos-MacBook-Pro.local>
2017-03-13 12:21:49 -0600

    add db comms

--- condominios/views.py
+++ condominios/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
 import os
 import json
+import psycopg2
+import pyodbc
 
 module_dir = os.path.dirname(__file__)  # get current directory
 
 # Create your views here.
 def home(request):
     context = {}
+
+    server = '200.23.150.13\SQL2008'
+    database = 'Cintermex_Prod'
+    username = 'sa'
+    password = '2333'
+    driver = '{SQL Server Native Client 11.0}'
+    cnxn = pyodbc.connect(
+        'DRIVER=' + driver + ';PORT=61451;SERVER=' + server + ';PORT=61451;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+
+    try:
+        #conn = psycopg2.connect(database="Cintermex_Prod\SQL2008", user="sa", host="200.23.150.13", password="2333")
+        #conn.close()
+
+        server = '200.23.150.13'
+        database = 'Cintermex_Prod'
+        username = 'sa'
+        password = '2333'
+        driver = '{ODBC Driver 13 for SQL Server}'
+        cnxn = pyodbc.connect(
+            'DRIVER=' + driver + ';PORT=61451;SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+
+        print "We are happy!"
+    except:
+        print "we are not happy :("
+
+
     return render(request, "index.html", context)
 
 

ae11a55155b10c8cc0ba6d6340be232234c95431
Alexstrider <alexrum92@gmail.com>
2017-04-11 11:48:38 -0500

    Se despliegan los datos de avisos, Negocios Permanentes, Disponibilidad de datos cargados de la base de datos.

--- condominios/views.py
+++ condominios/views.py
@@ -17,8 +17,6 @@ def home(request):
     context = {}
 
     try:
-        # conn = psycopg2.connect(database="Condominos", user="DESKTOP-5P46H40\alexr", host="localhost", password="")
-        # conn.close()
 
         server = 'DESKTOP-5P46H40'
         database = 'Condominos'
@@ -27,6 +25,21 @@ def home(request):
         driver = '{ODBC Driver 13 for SQL Server}'
         cnxn = pyodbc.connect(
             'DRIVER=' + driver + ';PORT=61451;SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+        
+        cur = cnxn.cursor()
+        
+        querystring = "SELECT TOP (2) Nombre, Descripcion FROM AvisoPagina ORDER BY Id DESC"
+        cur.execute(querystring)
+        listaAvisos = cur.fetchall()
+        
+        avisos = []
+        
+        
+        for aviso in listaAvisos:
+            avisos.append({"nombre":aviso[0], "descripcion":aviso[1]})
+
+        context["avisos"] = avisos
+        cur.close()
 
         print "We are happy!"
     except:
@@ -49,6 +62,24 @@ def avisos(request):
             'DRIVER=' + driver + ';PORT=61451;SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
 
         print "We are happy!"
+        
+        cur = cnxn.cursor()
+        
+        querystring = "SELECT TOP (8) Nombre, Descripcion FROM AvisoPagina ORDER BY Id DESC"
+        cur.execute(querystring)
+        listaAvisos = cur.fetchall()
+        
+        avisos = []
+        
+        for aviso in listaAvisos:
+            avisos.append({"nombre":aviso[0], "descripcion":aviso[1]})
+
+        context["avisos"] = avisos
+        
+        
+
+        cur.close()
+        
     except:
         print "we are not happy :("
     
@@ -72,61 +103,58 @@ def negocios_permanentes(request):
         cur = cnxn.cursor()
 
         cur.execute("{CALL WebCondominosABC (?, ?)}", ("A", "Z"))
-        print(cur.fetchall())
+        negociosLista = cur.fetchall()
+        
+        context['alphabet'] = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+        
+        business = []
+
+        p_actividades = re.compile("Actividades: </b>(.*)</td>")
+        p_email = re.compile("href=mailto:([\w+\.]+@[\w+\.]+)>")
+        p_telefono = re.compile("Teléfono:\W+</b>([0-9+\(\)-]+)</td>")
+        p_website = re.compile("Website: </b><a href=(.*)target=_blank>")
+        p_local = re.compile("Local=[0-9]+ target=_self>(.*)</a><tr>")
+        p_local_2 = re.compile("Nivel=(.*)&Local=([0-9]+)")
+
+        for child in negociosLista:
+            data = child[2]
+            element = {'name': child[1]}
+
+            match = p_actividades.search(data)
+            if match:
+                element['actividades'] = match.group(1)
+
+            match = p_email.search(data)
+            if match:
+                element['email'] = match.group(1)
+
+            match = p_telefono.search(data)
+            if match:
+                element['telefono'] = match.group(1)
+
+            match = p_website.search(data)
+            if match:
+                element['website'] = match.group(1)
+
+            match = p_local.search(data)
+            if match:
+                element['local'] = match.group(1)
+
+            match = p_local_2.search(data)
+            if match:
+                element['local'] = match.group(1) + " " + match.group(2)
+
+            business.append(element)
+
+        context['business_data'] = business
+        
 
         cur.close()
-    #querystring = "SELECT * FROM AvisoMasivo"
-        
-        #cur.execute(querystring)
-        #cnxn.commit()
+
     except:
         print "we are not happy :("
 
-    context['alphabet'] = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
-
-    business = []
-
-    file_path = os.path.join(module_dir, '../condominios/static/xml/Permanentes.xml')
-    e = xml.etree.ElementTree.parse(file_path).getroot()
-
-    p_actividades = re.compile("Actividades: </b>(.*)</td>")
-    p_email = re.compile("href=mailto:([\w+\.]+@[\w+\.]+)>")
-    p_telefono = re.compile("Teléfono:\W+</b>([0-9+\(\)-]+)</td>")
-    p_website = re.compile("Website: </b><a href=(.*)target=_blank>")
-    p_local = re.compile("Local=[0-9]+ target=_self>(.*)</a><tr>")
-    p_local_2 = re.compile("Nivel=(.*)&Local=([0-9]+)")
-
-    for child in e:
-        data = child.find('Condomino').text
-        element = {'name': child.find('NomPlanosOcu54').text}
-
-        match = p_actividades.search(data)
-        if match:
-            element['actividades'] = match.group(1)
-
-        match = p_email.search(data)
-        if match:
-            element['email'] = match.group(1)
-
-        match = p_telefono.search(data)
-        if match:
-            element['telefono'] = match.group(1)
-
-        match = p_website.search(data)
-        if match:
-            element['website'] = match.group(1)
-
-        match = p_local.search(data)
-        if match:
-            element['local'] = match.group(1)
-
-        match = p_local_2.search(data)
-        if match:
-            element['local'] = match.group(1) + " " + match.group(2)
-
-        business.append(element)
-
-        context['business_data'] = business
+    
 
     return render(request, "negocios_permanentes.html", context)
 
@@ -148,55 +176,57 @@ def disponibilidad(request):
         driver = '{ODBC Driver 13 for SQL Server}'
         cnxn = pyodbc.connect(
             'DRIVER=' + driver + ';PORT=61451;SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+        
+        cur = cnxn.cursor()
 
-        print "We are happy!"
+        cur.execute("{CALL WebCondominosDisponibles (?, ?)}", ("0", "10000"))
+        disponiblesLista = cur.fetchall()
+        
+        spaces = []
+
+        p_local = re.compile("<b>Local Vacante: (.*)</b></td>")
+        p_status = re.compile("<b>Status: </b>(.*)</td>\n</tr>")
+        p_metros = re.compile("<b>Metros cuadrados: </b>([0-9.]+)</td></tr>")
+        p_contacto = re.compile("<b>Contacto: </b>(.*)</td></tr><tr><td><b>Tel")
+        p_telefono = re.compile("fono: </b>([0-9-\W\(\)\+]+)</td></tr>")
+        p_comentarios = re.compile("Comentarios: </b>(.*)</td></tr>")
+
+        for child in disponiblesLista:
+            data = child[4]
+            element = {}
+
+            match = p_local.search(data)
+            if match:
+                element['local'] = match.group(1)
+
+            match = p_status.search(data)
+            if match:
+                element['status'] = match.group(1)
+
+            match = p_metros.search(data)
+            if match:
+                element['metros'] = match.group(1)
+
+            match = p_contacto.search(data)
+            if match:
+                element['contacto'] = match.group(1)
+
+            match = p_telefono.search(data)
+            if match:
+                element['telefono'] = match.group(1)
+
+            match = p_comentarios.search(data)
+            if match:
+                element['comentarios'] = match.group(1)
+
+            spaces.append(element)
+
+        context['spaces_data'] = spaces
+        
+        
     except:
         print "we are not happy :("
      
-
-    spaces = []
-
-    file_path = os.path.join(module_dir, '../condominios/static/xml/Disponibles.xml')
-    e = xml.etree.ElementTree.parse(file_path).getroot()
-
-    p_local = re.compile("<b>Local Vacante: (.*)</b></td>")
-    p_status = re.compile("<b>Status: </b>(.*)</td>\n</tr>")
-    p_metros = re.compile("<b>Metros cuadrados: </b>([0-9.]+)</td></tr>")
-    p_contacto = re.compile("<b>Contacto: </b>(.*)</td></tr><tr><td><b>Tel")
-    p_telefono = re.compile("fono: </b>([0-9-\W\(\)\+]+)</td></tr>")
-    p_comentarios = re.compile("Comentarios: </b>(.*)</td></tr>")
-
-    for child in e:
-        data = child.find('Condomino').text
-        element = {}
-
-        match = p_local.search(data)
-        if match:
-            element['local'] = match.group(1)
-
-        match = p_status.search(data)
-        if match:
-            element['status'] = match.group(1)
-
-        match = p_metros.search(data)
-        if match:
-            element['metros'] = match.group(1)
-
-        match = p_contacto.search(data)
-        if match:
-            element['contacto'] = match.group(1)
-
-        match = p_telefono.search(data)
-        if match:
-            element['telefono'] = match.group(1)
-
-        match = p_comentarios.search(data)
-        if match:
-            element['comentarios'] = match.group(1)
-
-        spaces.append(element)
-
-        context['spaces_data'] = spaces
 
     return render(request, "disponibilidad.html", context)
 
